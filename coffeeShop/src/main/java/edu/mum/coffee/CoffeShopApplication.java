13c29732f575eb1efaaa60846af5239ea23b8fce
Firew_Abebe <abebe.firew9@gmail.com>
2017-08-05 00:51:35 -0500

    Basically there is not change!

--- coffeeShop/src/main/java/edu/mum/coffee/CoffeShopApplication.java
+++ coffeeShop/src/main/java/edu/mum/coffee/CoffeShopApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan("edu.mum")
 public class CoffeShopApplication {
-
+// This is the application class that starts the project
 	public static void main(String[] args) {
 		SpringApplication.run(CoffeShopApplication.class, args);
 	}
